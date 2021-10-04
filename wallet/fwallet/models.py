19a00dfd8b0ab44142910f01265823f6beafcdfe
Joportus <81198811+Joportus@users.noreply.github.com>
2021-10-03 21:18:59 -0300

    creada base de dayps para ingresos y gastos

--- wallet/fwallet/models.py
+++ wallet/fwallet/models.py
@@ -1,9 +1,28 @@
+
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
+#id, monto, tipo(ingreso, gasto, deuda), nombre(si es que es gasto, a quien le pagaste si es que es ingreso, quien te pago)
+#fecha, clases de gasto, descripción
+
+#Consultas sql para filtrar y metodos para ingresar datos a la base de datos
 
 # Create your models here.
 
 class User(AbstractUser):
     apodo = models.CharField(max_length=30)
 
+small_size = 20
+medium_size = 1000
+class RegistroDinero(models.Model):
+
+    id = models.IntegerField(primary_key=True)
+    monto = models.IntegerField()
+    tipos_de_gasto = [('i','ingreso'), ('g','gasto'), ('d','deuda')]
+    tipo = models.CharField(max_length=small_size, choices=tipos_de_gasto)
+    nombre = models.CharField(max_length = small_size)
+    fecha = models.DateField()
+    clase = models.CharField(max_length=small_size)
+    descripcion = models.CharField(max_length=medium_size)
 
