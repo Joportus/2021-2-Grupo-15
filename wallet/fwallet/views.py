bbb56f6fdbda52aa51446d9dcbbf0a8c2fd271a1
Joportus <81198811+Joportus@users.noreply.github.com>
2021-10-03 21:57:44 -0300

    creacion de funcion para filtrar registros por monto

--- wallet/fwallet/views.py
+++ wallet/fwallet/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render, redirect
-
-
+from django.http import HttpResponse
+from wallet.fwallet.models import RegistroDinero
 
 # Create your views here.
 def walletv(request):
 
     dict = {"ingresos":"ingresos", "gastos": "gastos"}
     return render(request, "fwallet\wallet.html", dict)
+
+#Busca un registro de ingreso/gasto filtrando por el monto
+def buscar(request):
+    input1_name = "monto"
+
+    pagina_resultado = "resultados_busq_monto.html"
+
+    if request.GET[input1_name]:
+
+        monto_ingresado = request.GET["monto_ingresado"]
+        registro = RegistroDinero.objects.filter(monto__icontrains=monto_ingresado)
+
+        return render(request, pagina_resultado, {"registro":registro, "query":monto_ingresado})
+
+    else:
+
+        mensaje = "No se introdujo nada"
+
+    return HttpResponse(mensaje)
+
+
